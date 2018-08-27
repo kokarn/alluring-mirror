0bb289292996cf2c9fabc520877846f8f08cf4e6
Oskar Risberg <oskar.risberg@stendahls.se>
2018-08-27 19:29:08 +0200

    Update name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: "server",
+            name: "alluring-mirror",
             script: "./server.js",
             cwd: "/home/pi/alluring-mirror/current",
             env: {
