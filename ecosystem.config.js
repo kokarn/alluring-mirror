03f80ea98d5c63a4f64c4f8988eb99e4f90e9189
Oskar Risberg <oskar.risberg@stendahls.se>
2018-08-28 11:57:58 +0200

    Reload the server on config.json changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
             name: 'alluring-mirror',
             script: './server.js',
             cwd: '/home/pi/alluring-mirror/current',
+            watch: 'config.json'
             env: {
                 'NODE_ENV': 'development',
             },
