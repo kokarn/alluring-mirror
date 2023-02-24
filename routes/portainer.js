076bf601a01f88fc216ae887d8fb7443798f7dd9
Oskar Risberg <oskar.risberg@stendahls.se>
2023-02-24 12:23:00 +0100

    loop multiple environments

--- routes/portainer.js
+++ routes/portainer.js
@@ -5,14 +5,28 @@ module.exports = async function(request, response) {
         return response.send('Portainer not configured');
     }
     let containers;
+    let up = 0;
+    let down = 0;
+
     try {
         containers = await portainerClient('/api/endpoints/2/docker/containers/json', 'get');
     } catch (requestError) {
         console.log(requestError);
     }
 
-    let up = 0;
-    let down = 0;
+    containers.map((container) => {
+        if (container.State === 'running') {
+            up = up + 1;
+        } else {
+            down = down + 1;
+        }
+    });
+
+    try {
+        containers = await portainerClient('/api/endpoints/3/docker/containers/json', 'get');
+    } catch (requestError) {
+        console.log(requestError);
+    }
 
     containers.map((container) => {
         if (container.State === 'running') {
