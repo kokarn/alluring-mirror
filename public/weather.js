362f1135b5da167285439bd7ac79bb761e77772c
Oskar Risberg <oskar.risberg@stendahls.se>
2026-01-06 23:02:24 +0100

    Add SMHI warnings

--- public/weather.js
+++ public/weather.js
@@ -1,46 +1,73 @@
-(function(){
+(function () {
     const maxDays = 5;
 
-    function setWeather(){
+    function setWeather() {
         $.ajax({
             url: 'weather/',
         })
-        .then( function( weather ){
-            html =
-            '<div class="current-weather-wrapper">' +
-                '<i class="icon-' + weather.now.code + '"></i> ' +
-                '<span>' + Math.ceil( weather.now.temperature ) + '&deg;C</span>' +
-            '</div>';
+            .then(function (weather) {
+                html = '';
 
-            html = html + '<div class="foreacasts-wrapper">';
+                let warningsHtml = '';
+                if (weather.warnings && weather.warnings.length > 0) {
+                    warningsHtml += '<div class="weather-warnings">';
+                    weather.warnings.forEach(function (warning) {
+                        const start = new Date(warning.approximateStart);
+                        const end = new Date(warning.approximateEnd);
+                        const formatTime = (d) => d.getHours().toString().padStart(2, '0') + ':' + d.getMinutes().toString().padStart(2, '0');
+                        const formatDate = (d) => d.toLocaleDateString('sv-SE', { weekday: 'short', day: 'numeric', month: 'short' });
 
-            var numDays = 0;
-            for( var date in weather.forecast ) {
-                const dateObject = new Date( date + 'T03:24:00' );
-                html = html + '<div class="foreacast-wrapper">';
-                html = html + '<i class="icon-' + weather.forecast[ date ].code + '"></i>';
-                html = html + '<div class="forecast-weather-wrapper">' + dateObject.toLocaleString( 'en-us', {  weekday: 'short' } ) + '<br>' + Math.ceil( weather.forecast[ date ].high ) + '&deg;C</div>';
-                html = html + '</div>';
-                numDays = numDays + 1;
+                        let displayRange = '';
+                        if (start.toDateString() === end.toDateString()) {
+                            displayRange = formatDate(start) + ' ' + formatTime(start) + ' - ' + formatTime(end);
+                        } else {
+                            displayRange = formatDate(start) + ' ' + formatTime(start) + ' - ' + formatDate(end) + ' ' + formatTime(end);
+                        }
 
-                if ( numDays >= maxDays ) {
-                    break;
+                        warningsHtml += '<div class="weather-warning level-' + warning.levelCode.toLowerCase() + '">';
+                        warningsHtml += warning.areaName + ': ' + warning.description + '<br>';
+                        warningsHtml += '<small>' + displayRange + '</small>';
+                        warningsHtml += '</div>';
+                    });
+                    warningsHtml += '</div>';
                 }
-            }
 
-            html = html + '</div>';
+                html +=
+                    '<div class="current-weather-wrapper">' +
+                    '<i class="icon-' + weather.now.code + '"></i> ' +
+                    '<span>' + Math.ceil(weather.now.temperature) + '&deg;C</span>' +
+                    '</div>';
 
-            $( '.js-weather' ).html(html);
-        } )
-        .catch( function( promiseError ){
-            $( '.js-weather' ).html( '<pre>' + JSON.stringify( promiseError, null, 4 ) + '</pre>' );
-            console.log( promiseError );
-        } );
+                html = html + '<div class="foreacasts-wrapper">';
+
+                var numDays = 0;
+                for (var date in weather.forecast) {
+                    const dateObject = new Date(date + 'T03:24:00');
+                    html = html + '<div class="foreacast-wrapper">';
+                    html = html + '<i class="icon-' + weather.forecast[date].code + '"></i>';
+                    html = html + '<div class="forecast-weather-wrapper">' + dateObject.toLocaleString('en-us', { weekday: 'short' }) + '<br>' + Math.ceil(weather.forecast[date].high) + '&deg;C</div>';
+                    html = html + '</div>';
+                    numDays = numDays + 1;
+
+                    if (numDays >= maxDays) {
+                        break;
+                    }
+                }
+
+                html = html + '</div>';
+                html = html + warningsHtml;
+
+                $('.js-weather').html(html);
+            })
+            .catch(function (promiseError) {
+                $('.js-weather').html('<pre>' + JSON.stringify(promiseError, null, 4) + '</pre>');
+                console.log(promiseError);
+            });
     }
 
-    $(function() {
+    $(function () {
         setWeather();
 
-        setInterval( setWeather, 60000 );
+        setInterval(setWeather, 60000);
     });
 })();
--- routes/weather.js
+++ routes/weather.js
@@ -1,13 +1,36 @@
-const got = require( 'got' );
-const moment = require( 'moment' );
+const got = require('got');
+const moment = require('moment');
 
-const config = require( '../data/config.json' );
+const config = require('../data/config.json');
 
 const WEATHER_ENDPOINT = 'https://api.openweathermap.org/data/2.5';
 
 const WEATHER_LOCATION = {
-    gothenburg: '2711537',
-    brastad: '2719670',
+    gothenburg: {
+        id: '2711537',
+        lat: 57.7089,
+        lon: 11.9746
+    },
+    brastad: {
+        id: '2719670',
+        lat: 58.3847,
+        lon: 11.4883
+    },
+};
+
+const WARNINGS_ENDPOINT = 'https://opendata-download-warnings.smhi.se/ibww/api/version/1/warning.json';
+
+const isPointInPolygon = (lat, lon, polygon) => {
+    let inside = false;
+    for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+        const xi = polygon[i][0], yi = polygon[i][1]; // SMHI coordinates are [lon, lat]
+        const xj = polygon[j][0], yj = polygon[j][1];
+
+        const intersect = ((yi > lat) !== (yj > lat))
+            && (lon < (xj - xi) * (lat - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    return inside;
 };
 
 const WEATHER_MAP = {
@@ -78,85 +101,137 @@ const WEATHER_MAP = {
     '804': 'cloudy',
 };
 
-const getWeatherCode = function getWeatherCode( weatherPoint ) {
-    if ( typeof WEATHER_MAP[ weatherPoint.weather[ 0 ].id ] === 'object' ) {
-        return WEATHER_MAP[ weatherPoint.weather[ 0 ].id ][ weatherPoint.weather[ 0 ].icon ];
+const getWeatherCode = function getWeatherCode(weatherPoint) {
+    if (typeof WEATHER_MAP[weatherPoint.weather[0].id] === 'object') {
+        return WEATHER_MAP[weatherPoint.weather[0].id][weatherPoint.weather[0].icon];
     }
 
-    return WEATHER_MAP[ weatherPoint.weather[ 0 ].id ];
+    return WEATHER_MAP[weatherPoint.weather[0].id];
 };
 
-module.exports = function( request, response ){
-    const returnData = {};
+module.exports = function (request, response) {
+    const returnData = {
+        warnings: []
+    };
 
-    got( `${ WEATHER_ENDPOINT }/weather`, {
+    const location = WEATHER_LOCATION[config.weather];
+
+    got(`${WEATHER_ENDPOINT}/weather`, {
         json: true,
         query: {
             appid: process.env.OPEN_WEATHER_MAP,
-            id: WEATHER_LOCATION[ config.weather ],
+            id: location.id,
             units: 'metric',
         },
-    } )
-    .then( ( weatherReturnData ) => {
-        returnData.now = {
-            code: getWeatherCode( weatherReturnData.body ),
-            // temperature: weatherReturnData.body.main.temp,
-            temperature: weatherReturnData.body.main.feels_like,
-            high: weatherReturnData.body.main.temp_max,
-            low: weatherReturnData.body.main.temp_min,
-        };
+    })
+        .then((weatherReturnData) => {
+            returnData.now = {
+                code: getWeatherCode(weatherReturnData.body),
+                // temperature: weatherReturnData.body.main.temp,
+                temperature: weatherReturnData.body.main.feels_like,
+                high: weatherReturnData.body.main.temp_max,
+                low: weatherReturnData.body.main.temp_min,
+            };
 
-        return got( `${ WEATHER_ENDPOINT }/forecast`, {
-            json: true,
-            query: {
-                appid: process.env.OPEN_WEATHER_MAP,
-                id: WEATHER_LOCATION[ config.weather ],
-                units: 'metric',
-            },
-        } );
-    } )
-    .then( ( forecastData ) => {
-        const measurements = [];
+            return got(`${WEATHER_ENDPOINT}/forecast`, {
+                json: true,
+                query: {
+                    appid: process.env.OPEN_WEATHER_MAP,
+                    id: location.id,
+                    units: 'metric',
+                },
+            });
+        })
+        .then((forecastData) => {
+            const measurements = [];
 
-        for ( const weatherPoint of forecastData.body.list ) {
-            const date = moment( weatherPoint.dt * 1000 );
+            for (const weatherPoint of forecastData.body.list) {
+                const date = moment(weatherPoint.dt * 1000);
 
-            measurements.push( {
-                date: date.format( 'YYYY-MM-DD' ),
-                time: date.format( 'HH:mm' ),
-                code: getWeatherCode( weatherPoint ),
-                temperature: weatherPoint.main.temp,
-            } );
-        }
-
-        const forecast = {};
-
-        for ( const measurement of measurements ) {
-            if ( !forecast[ measurement.date ] ) {
-                forecast[ measurement.date ] = {
-                    code: measurement.code,
-                    temperature: measurement.temperature,
-                    high: measurement.temperature,
-                    low: measurement.temperature,
-                };
+                measurements.push({
+                    date: date.format('YYYY-MM-DD'),
+                    time: date.format('HH:mm'),
+                    code: getWeatherCode(weatherPoint),
+                    temperature: weatherPoint.main.temp,
+                });
             }
 
-            if ( forecast[ measurement.date ].high < measurement.temperature ) {
-                forecast[ measurement.date ].high = measurement.temperature;
+            const forecast = {};
+
+            for (const measurement of measurements) {
+                if (!forecast[measurement.date]) {
+                    forecast[measurement.date] = {
+                        code: measurement.code,
+                        temperature: measurement.temperature,
+                        high: measurement.temperature,
+                        low: measurement.temperature,
+                    };
+                }
+
+                if (forecast[measurement.date].high < measurement.temperature) {
+                    forecast[measurement.date].high = measurement.temperature;
+                }
+
+                if (forecast[measurement.date].low > measurement.temperature) {
+                    forecast[measurement.date].low = measurement.temperature;
+                }
             }
 
-            if ( forecast[ measurement.date ].low > measurement.temperature ) {
-                forecast[ measurement.date ].low = measurement.temperature;
-            }
-        }
+            returnData.forecast = forecast;
 
-        returnData.forecast = forecast;
-    } )
-    .then( () => {
-        response.send( returnData );
-    } )
-    .catch( ( requestError ) => {
-        console.error( requestError );
-        response.send( requestError );
-    } );
+            // Fetch SMHI Warnings
+            return got(WARNINGS_ENDPOINT, { json: true });
+        })
+        .then((warningsReturn) => {
+            const warnings = warningsReturn.body;
+            if (!Array.isArray(warnings)) {
+                return;
+            }
+
+            for (const warning of warnings) {
+                if (!warning.warningAreas) {
+                    continue;
+                }
+
+                for (const area of warning.warningAreas) {
+                    let isInside = false;
+                    if (!area.area || !area.area.geometry) {
+                        continue;
+                    }
+
+                    const geometry = area.area.geometry;
+                    if (geometry.type === 'Polygon') {
+                        if (isPointInPolygon(location.lat, location.lon, geometry.coordinates[0])) {
+                            isInside = true;
+                        }
+                    } else if (geometry.type === 'MultiPolygon') {
+                        for (const polygon of geometry.coordinates) {
+                            if (isPointInPolygon(location.lat, location.lon, polygon[0])) {
+                                isInside = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (isInside) {
+                        returnData.warnings.push({
+                            id: area.id,
+                            levelCode: area.warningLevel.code,
+                            levelName: area.warningLevel.sv,
+                            description: area.eventDescription.sv,
+                            areaName: area.areaName.sv,
+                            approximateStart: area.approximateStart,
+                            approximateEnd: area.approximateEnd
+                        });
+                    }
+                }
+            }
+        })
+        .catch((warningError) => {
+            console.error('Error fetching SMHI warnings:', warningError.message);
+            // Don't fail the whole request just because warnings failed
+        })
+        .finally(() => {
+            response.send(returnData);
+        });
 };
